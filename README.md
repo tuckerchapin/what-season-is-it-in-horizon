ba1b91c65555683730ca386164d9bfcf83135e6b
Tucker Chapin <tuckerchapin@tuckerbook-air.local>
2018-11-13 15:53:25 -0500

    Times are good.

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# what-season-is-it-in-forza
+# what-season-is-it-in-horizon
 Simple site to show you the current season in Forza Horizon 4
---
+++ seasons.js
@@ -0,0 +1,26 @@
+const seasons = ["winter", "spring", "summer", "autumn"];
+const winterStarts = new Date(Date.UTC(2018, 10, 8, 14, 30, 00));
+const now = new Date();
+
+// Calculate the number of seasons (weeks) since the seed time.
+let numberOfSeasonsPassed = Math.floor(Math.abs(
+    (now - winterStarts) / (7 * 24 * 60 * 60 * 1000)
+));
+
+let currentSeason = seasons[numberOfSeasonsPassed % 4];
+
+// Calculate the next season.
+let nextSeason = seasons[(numberOfSeasonsPassed + 1) % 4];
+let nextSeasonStart = winterStarts;
+nextSeasonStart.setDate(nextSeasonStart.getDate() + ((numberOfSeasonsPassed + 1) * 7));
+
+// How much time until the next season.
+let timeTilNextSeason = nextSeasonStart - now;
+let daysTilNextSeason = Math.floor(timeTilNextSeason / (24 * 60 * 60 * 1000));
+let hoursTilNextSeason = Math.floor((timeTilNextSeason / (60 * 60 * 1000)) - (daysTilNextSeason * 24));
+let minutesTilNextSeason = Math.floor((timeTilNextSeason / (60 * 1000)) - (hoursTilNextSeason * 60) - (daysTilNextSeason * 24 * 60));
+
+// Format the time for display
+let formattedTimeTil = `${daysTilNextSeason}D ${hoursTilNextSeason}H ${minutesTilNextSeason}M`
+
+document.title = "It's " + currentSeason + " in Horizon";
