c258454720e436470d1e1d4e0817138f1678f5ad
Tucker Chapin <tuckerchapin@tuckerbook-air.local>
2018-11-14 21:31:24 -0500

    Moved around.

--- content.js
+++ content.js
@@ -1,28 +1,31 @@
 // List of the uploaded user's images
 const userBackgrounds = {
     "winter": [
-        "kml-1.jpg",
-        "parkel421.jpg",
-        "parkel422.jpg",
-        "parkel423.jpg",
-        "CCXGT1.png",
-        "naixu1.png",
+        "kml-1",
+        "parkel421",
+        "parkel422",
+        "parkel423",
+        "CCXGT1",
+        "naixu1",
     ],
     "spring": [
     ],
     "summer": [
     ],
     "autumn": [
-        "SkylineSonata1.jpg",
+        "SkylineSonata1",
     ],
 };
 
 document.getElementById("season-splash").src = `images/${currentSeason}/${currentSeason}-splash.svg`;
 
-// Randomly select a background appropraite for the season
 if (userBackgrounds[currentSeason].length > 0) {
+    // Randomly select a background appropriate for the season
     let selectedBackground = userBackgrounds[currentSeason][Math.floor(Math.random() * userBackgrounds[currentSeason].length)];
-    document.getElementById("season-background").src = `images/${currentSeason}/backgrounds/${selectedBackground}`;
+    document.getElementById("season-background").src = `images/${currentSeason}/backgrounds/${selectedBackground}.jpg`;
+
+    // Art credit
+    console.log("Background image courtesy of /u/" + selectedBackground.slice(0, -1));
 } else {
     document.getElementById("season-background").src = `images/${currentSeason}/${currentSeason}-background-default.jpg`;
 }
@@ -31,6 +34,3 @@ document.getElementById("days").textContent = daysTilNextSeason;
 document.getElementById("hours").textContent = hoursTilNextSeason;
 document.getElementById("minutes").textContent = hoursTilNextSeason;
 document.getElementById("countdown-title").textContent = nextSeason + " starts in";
-
-// Art credit
-console.log("Background image courtesy of " + selectedBackground.replace(/\.[^/.]+$/, "").slice(0, -1));
