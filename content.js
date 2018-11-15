0e05f8604cc7660eb0d2ffdc16b38f17d0f857d3
Tucker Chapin <tuckerchapin@tuckerbook-air.local>
2018-11-14 21:18:59 -0500

    Added an autumn user background, credits, and fallback to a default.

--- content.js
+++ content.js
@@ -9,33 +9,28 @@ const userBackgrounds = {
         "naixu1.png",
     ],
     "spring": [
-        "spring"
     ],
     "summer": [
-        "summer"
     ],
     "autumn": [
-        "autumn"
+        "SkylineSonata1.jpg",
     ],
 };
 
 document.getElementById("season-splash").src = `images/${currentSeason}/${currentSeason}-splash.svg`;
 
 // Randomly select a background appropraite for the season
-let selectedBackground = userBackgrounds[currentSeason][Math.floor(Math.random() * userBackgrounds[currentSeason].length)];
-document.getElementById("season-background").src = `images/${currentSeason}/user/${selectedBackground}`;
+if (userBackgrounds[currentSeason].length > 0) {
+    let selectedBackground = userBackgrounds[currentSeason][Math.floor(Math.random() * userBackgrounds[currentSeason].length)];
+    document.getElementById("season-background").src = `images/${currentSeason}/backgrounds/${selectedBackground}`;
+} else {
+    document.getElementById("season-background").src = `images/${currentSeason}/${currentSeason}-background-default.jpg`;
+}
 
 document.getElementById("days").textContent = daysTilNextSeason;
 document.getElementById("hours").textContent = hoursTilNextSeason;
 document.getElementById("minutes").textContent = hoursTilNextSeason;
 document.getElementById("countdown-title").textContent = nextSeason + " starts in";
 
-// let countdownContainer = document.getElementById("countdown-container");
-// let blurredBackground = document.createElement("img");
-// blurredBackground.id = "blurred-background";
-// blurredBackground.src = `images/${currentSeason}/user/${selectedBackground}`;
-// // console.log(countdownContainer.offsetWidth);
-// // console.log(blurredBackground.style.width);
-// blurredBackground.style.width = countdownContainer.offsetWidth + "px";
-// blurredBackground.style.height = countdownContainer.offsetHeight + "px";
-// document.body.insertBefore(blurredBackground, document.getElementById("countdown-container"));
+// Art credit
+console.log("Background image courtesy of " + selectedBackground.replace(/\.[^/.]+$/, "").slice(0, -1));
